ef9e19de5eb3b9183b17fcda416a8acfdd618e58
David Ingraham <davingrahamd@gmail.com>
2018-12-30 15:27:47 -0700

    Disable Firmware update Prompt

--- MainV2.cs
+++ MainV2.cs
@@ -1533,6 +1533,7 @@ namespace MissionPlanner
                         _connectionControl.TOOL_APMFirmware.Items.IndexOf(Firmwares.ArduPlane);
                 }
 
+                /* Don't try to auto update firmware for Cobalt Version
                 // check for newer firmware
                 var softwares = Firmware.LoadSoftwares();
 
@@ -1569,6 +1570,7 @@ namespace MissionPlanner
                         log.Error(ex);
                     }
                 }
+                */
 
                 FlightData.CheckBatteryShow();
 
