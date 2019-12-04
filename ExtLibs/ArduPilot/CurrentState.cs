1622c984971ceb680492ab1128143a722ec8cb03
Michael Oborne <mich146@hotmail.com>
2019-12-04 16:15:19 +0800

    MAVLinkInterface: propagate radio/status across all mavs on the link

--- ExtLibs/ArduPilot/CurrentState.cs
+++ ExtLibs/ArduPilot/CurrentState.cs
@@ -1632,7 +1632,10 @@ namespace MissionPlanner
 
         private void Parent_OnPacketReceived(object sender, MAVLink.MAVLinkMessage mavLinkMessage)
         {
-            if (mavLinkMessage.sysid == parent.sysid && mavLinkMessage.compid == parent.compid)
+            if (mavLinkMessage.sysid == parent.sysid && mavLinkMessage.compid == parent.compid 
+                || mavLinkMessage.msgid == (uint)MAVLink.MAVLINK_MSG_ID.RADIO // propagate the RADIO/RADIO_STATUS message across all devices on this link
+                || mavLinkMessage.msgid == (uint)MAVLink.MAVLINK_MSG_ID.RADIO_STATUS)
+            {
                 switch (mavLinkMessage.msgid)
                 {
                     case (uint) MAVLink.MAVLINK_MSG_ID.RC_CHANNELS_SCALED:
@@ -2892,6 +2895,7 @@ namespace MissionPlanner
                     }
                         break;
                 }
+            }
         }
 
         public event EventHandler csCallBack;
