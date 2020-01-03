1f2728ed74fa2a3546409616fa82db69307de574
Pierre Kancir <pierre.kancir.emn@gmail.com>
2020-01-03 15:13:54 +0100

    Update MainV2.cs to correctly check for windows XP
    another solution would be to use RuntimeInformation.IsOSPlatform(OSPlatform.Windows) but that bring another dependency

--- MainV2.cs
+++ MainV2.cs
@@ -3224,8 +3224,11 @@ namespace MissionPlanner
             MissionPlanner.Utilities.Tracking.AddTiming("AppLoad", "Load Time",
                 (DateTime.Now - Program.starttime).TotalMilliseconds, "");
 
-            bool winXp = Environment.OSVersion.Version.Major == 5;
             /* Disable Update checking in SpektreWorks Builds 
+            bool winXp = Environment.OSVersion.Version.Major == 5;
+            int p = (int) Environment.OSVersion.Platform;
+            bool isWin = (p != 4) && (p != 6) && (p != 128);
+            bool winXp = isWin && Environment.OSVersion.Version.Major == 5;
             if (winXp)
             {
                 Common.MessageShowAgain("Windows XP",
