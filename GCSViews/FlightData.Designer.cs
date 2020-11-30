2452957e2ae213c4eece62870fdc1841b2fb5c55
Dan Laks <dan@spektreworks.com>
2020-11-30 12:56:47 -0700

    Increase alert_high for VPS Battery QuickView

--- GCSViews/FlightData.Designer.cs
+++ GCSViews/FlightData.Designer.cs
@@ -1187,7 +1187,7 @@
             // QUICK_VPSBattV
             // 
             this.QUICK_VPSBattV.alert_color = System.Drawing.Color.Red;
-            this.QUICK_VPSBattV.alert_high = 67.9D;
+            this.QUICK_VPSBattV.alert_high = 68.2D;
             this.QUICK_VPSBattV.alert_low = 46D;
             this.QUICK_VPSBattV.attention_color = System.Drawing.Color.Orange;
             this.QUICK_VPSBattV.attention_offset = 9D;
@@ -2343,7 +2343,7 @@
             this.windDir1.BackColor = System.Drawing.Color.Transparent;
             this.windDir1.DataBindings.Add(new System.Windows.Forms.Binding("Direction", this.bindingSource1, "wind_dir", true, System.Windows.Forms.DataSourceUpdateMode.Never));
             this.windDir1.DataBindings.Add(new System.Windows.Forms.Binding("Speed", this.bindingSource1, "wind_vel", true, System.Windows.Forms.DataSourceUpdateMode.Never));
-            this.windDir1.Direction = 360D;
+            this.windDir1.Direction = 180D;
             resources.ApplyResources(this.windDir1, "windDir1");
             this.windDir1.Name = "windDir1";
             this.windDir1.Speed = 0D;
